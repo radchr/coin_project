0bb4580745cd9f210883081692eb996bd9c47355
Radchenko Roman <taxcode2019@outlook.com>
2021-06-05 12:38:27 +0300

    add file py

---
+++ main.py
@@ -0,0 +1,26 @@
+#This example uses Python 2.7 and the python-request library.
+
+from requests import Request, Session
+from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
+import json
+
+url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+parameters = {
+  'start':'1',
+  'limit':'5000',
+  'convert':'USD'
+}
+headers = {
+  'Accepts': 'application/json',
+  'X-CMC_PRO_API_KEY': 'b17cbed5-42a2-449a-8610-ccaa79a4677c',
+}
+
+session = Session()
+session.headers.update(headers)
+
+try:
+  response = session.get(url, params=parameters)
+  data = json.loads(response.text)
+  print(data)
+except (ConnectionError, Timeout, TooManyRedirects) as e:
+  print(e)
